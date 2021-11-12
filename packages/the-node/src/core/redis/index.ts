182755850601a3a9dd0f40ea0ce2cd9568863a46
GarfDev <garfdev.13@gmail.com>
2021-11-12 12:13:26 +0700

    fix: add redis support

--- packages/the-node/src/core/redis/index.ts
+++ packages/the-node/src/core/redis/index.ts
@@ -5,6 +5,7 @@ import { Config } from 'config';
 let client: any = null;
 
 const createRedisClient = async () => {
+  console.log(Config.REDIS_URL);
   client = createClient({ url: Config.REDIS_URL
 });
   client.on('error', (err) => console.log('Redis Client Error', err));
